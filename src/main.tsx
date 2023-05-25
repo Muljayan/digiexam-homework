ac0c109cb51fa1ecc7b2a8a06f27fc303d903497
Emanuel Unge <emanuel.unge@digiexam.se>
2023-05-25 15:08:11 +0200

    fixing color schemes/themes so it's visible in enforced dark mode and light

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import App from './App.tsx'
 import {store} from './store/config.ts'
 import {Provider} from 'react-redux'
 import './index.css'
+import {createTheme, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: 'light'
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </ThemeProvider>
     </React.StrictMode>,
 )
